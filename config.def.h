ea80e5d0eb106a031dc520196aee345923d572f1
Alípio de Paula <alipiodepaula@gmail.com>
2026-04-13 23:39:42 -0300

    applied status2d patch

--- config.def.h
+++ config.def.h
@@ -12,8 +12,10 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static unsigned int baralpha        = 0xd0;
-static unsigned int borderalpha     = OPAQUE;
+
+/* Default opacity levels         fg      bg     border */
+unsigned int default_alphas[] = { OPAQUE, 0xd0U, OPAQUE };
+
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
